e4b9adc50c2e5fb9b884d83a565aa40275cd2ea4
Jade Meskill <jade.meskill@gmail.com>
2015-08-01 02:03:02 -0700

    add bin

--- pack.js
+++ pack.js
@@ -9,27 +9,31 @@
 //   crossy.io
 
 'use strict'
+var fs = require('fs');
 var tar = require('tar');
 var zlib = require('zlib');
 var path = require('path');
 var fstream = require('fstream');
 var packageJSON = require(path.resolve(process.cwd(), 'package.json'));
 
-var options =
-  path: path.resolve(process.cwd(), 'node_modules')
-  type: "Directory"
+function pack() {
+  var options = {
+    path: path.resolve(process.cwd(), 'node_modules'),
+    type: "Directory"
+  };
 
-filename = [
-  packageJSON.name,
-  packageJSON.version
-].join('-') + '.tar.gz'
+  var filename = [
+    packageJSON.name,
+    packageJSON.version
+  ].join('-') + '.tar.gz';
 
-output = fs.createWriteStream(filename);
+  var output = fs.createWriteStream(filename);
 
-fstream.Reader(options)
-  .pipe(tar.Pack())
-  .pipe(zlib.Gzip())
-  .pipe(output));
+  fstream.Reader(options)
+    .pipe(tar.Pack())
+    .pipe(zlib.Gzip())
+    .pipe(output);
+}
 
 module.exports = {
   pack: pack
